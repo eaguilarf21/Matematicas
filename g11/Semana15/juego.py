b8a2887f789b48f922fdaf72c42e7d189b28996e
eaguilarf21 <eaguilarf@certus.edu.pe>
2022-07-05 09:43:35 -0500

    PreS15

--- g11/Semana15/juego.py
+++ g11/Semana15/juego.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 
 pygame.init()
 
@@ -58,11 +59,40 @@ class Bala(pygame.sprite.Sprite):
         if self.rect.y<0:
             self.kill()
 
-        
+#crear piedra
+class Piedra(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("./Semana15/img/piedra.png")
+        self.image = pygame.transform.smoothscale(self.image,(60,60))
+        self.image.set_colorkey(negro)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(w)
+        self.rect.y = random.randrange(-140,-100)
+        self.speedx = random.randrange(-5,5)
+        self.speedy = random.randrange(1,10)
 
+    def update(self):
+        self.rect.x = self.rect.x + self.speedx
+        self.rect.y = self.rect.y + self.speedy
+        if self.rect.y>h or (self.rect.x>w or self.rect.x<0):
+            self.rect.x = random.randrange(w)
+            self.rect.y = random.randrange(-140,-100)
+            self.speedx = random.randrange(-5,5)
+            self.speedy = random.randrange(1,10)
+
+
+
+#grupo de juego
 all_sprites = pygame.sprite.Group()
+lista_meteorito = pygame.sprite.Group()
 player = Jugador()
 all_sprites.add(player)
+
+for contador in range(5):
+    meteorito = Piedra()
+    all_sprites.add(meteorito)
+    lista_meteorito.add(meteorito)
 
 #bucle del juego
 while True:
@@ -75,6 +105,11 @@ while True:
             if  eventoCapturado.key == pygame.K_SPACE:
                 player.dispara()
     
+    hits = pygame.sprite.spritecollide(player,lista_meteorito,True)
+    if hits:
+        pygame.quit()
+        sys.exit()
+
     #visualizar en pantalla
     all_sprites.update()
     pantalla.blit(fondo,(0,0))
